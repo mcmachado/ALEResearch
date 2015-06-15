9d5290dfc2778fe4fa80a3cc85e425e56d8d7eb4
alcinos <carion.nicolas@gmail.com>
2015-06-15 13:24:33 -0600

    Revert "working hash table on the weights"
    This reverts commit f5b1c2ba7798de364b8d7e36dcb51477376c4798.

--- src/agents/rl/sarsa/SarsaLearner.hpp
+++ src/agents/rl/sarsa/SarsaLearner.hpp
@@ -28,7 +28,7 @@ class SarsaLearner : public RLLearner<bool>{
 		vector<double> Q;               //Q(a) entries
 		vector<double> Qnext;           //Q(a) entries for next action
         vector<unordered_map<int,double> > e;      //Eligibility trace
-        vector<unordered_map<int,double> > w;      //Theta, weights vector
+		vector<vector<double> > w;      //Theta, weights vector
 
 		/**
  		* Constructor declared as private to force the user to instantiate SarsaLearner
