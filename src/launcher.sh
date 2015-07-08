4b7d680fde5e4200b77e5c5be3e586becbc9c052
alcinos <carion.nicolas@gmail.com>
2015-07-08 14:56:15 -0600

    better script

--- src/launcher.sh
+++ src/launcher.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 base_path=`pwd`
-
-cmd="./learner -s 3 -c conf.cfg -r /home/alcinos/roms/bank_heist.bin"
-
-for diff in `seq 0 3`;
+game="freeway"
+#romPath="/home/alcinos/roms/"
+romPath="/home/nicolas/Documents/Etudes/ENS/M1/Stage/emulateur/roms/"
+fullPath="${romPath}${game}.bin"
+cmd="./learner -s 3 -c conf.cfg -r ${fullPath}"
+echo "./mode_difficulty_getter ${fullPath} 1"
+for diff in `./mode_difficulty_getter ${fullPath} 1`;
 do
-    for m in `seq 0 5`
+    for mode in `./mode_difficulty_getter ${fullPath} 0`;
     do
-        mode=$((${m} * 4))
-        mkdir -p run_d${diff}_m${mode}
-        cp conf.cfg run_d${diff}_m${mode}/conf.cfg
-        echo "DIFFICULTY_LEVEL = ${diff}" >> run_d${diff}_m${mode}/conf.cfg
-        echo "GAME_MODE = ${mode}" >> run_d${diff}_m${mode}/conf.cfg
-        cp skeleton.pbs run_d${diff}_m${mode}/run_d${diff}_m${mode}.pbs
-        cp learner run_d${diff}_m${mode}
-        echo "cd ${base_path}/run_d${diff}_m${mode}" >>  run_d${diff}_m${mode}/run_d${diff}_m${mode}.pbs
-        echo "${cmd} &> out_d${diff}_m${mode}.out" >> run_d${diff}_m${mode}/run_d${diff}_m${mode}.pbs
+        folder="${game}_d${diff}_m${mode}"
+        mkdir -p ${folder}
+        cp conf.cfg ${folder}/conf.cfg
+        echo "DIFFICULTY_LEVEL = ${diff}" >> ${folder}/conf.cfg
+        echo "GAME_MODE = ${mode}" >> ${folder}/conf.cfg
+        cp skeleton.pbs ${folder}/run_d${diff}_m${mode}.pbs
+        cp learner ${folder}
+        echo "cd ${base_path}/${folder}" >>  ${folder}/run_d${diff}_m${mode}.pbs
+        echo "${cmd} &> out_${game}_d${diff}_m${mode}.out" >> ${folder}/run_d${diff}_m${mode}.pbs
+        bqsub ${folder}/run_d${diff}_m${mode}.pbs
     done
 done
+
+
+bqsub --submit
