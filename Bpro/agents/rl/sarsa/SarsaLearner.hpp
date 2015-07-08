523a75f480642fcfa4bd88fcf0f53dda7d5b8f97
YitaoLiang <yliang@fandm.edu>
2015-07-08 11:44:33 -0400

    Immigration to Hashtable

--- Bpro/agents/rl/sarsa/SarsaLearner.hpp
+++ Bpro/agents/rl/sarsa/SarsaLearner.hpp
@@ -14,28 +14,31 @@
 #include "../RLLearner.hpp"
 #endif
 #include <vector>
+#include <unordered_map>
 
 class SarsaLearner : public RLLearner{
 	private:
 		float alpha, delta, lambda, traceThreshold;
-		int numFeatures, currentAction, nextAction;
-		int toSaveWeightsAfterLearning, saveWeightsEveryXSteps, toSaveCheckPoint;
+		int currentAction, nextAction;
+        long long numFeatures;
+		int toSaveWeightsAfterLearning, saveWeightsEveryXFrames, toSaveCheckPoint;
 
 		std::string nameWeightsFile, pathWeightsFileToLoad;
         std::string checkPointName;
         std::string nameForLearningCondition;
         int episodePassed;
         int totalNumberFrames;
-        unsigned int maxFeatVectorNorm;
+        long long maxFeatVectorNorm;
+        int saveThreshold;
 
-		vector<int> F;					//Set of features active
-		vector<int> Fnext;              //Set of features active in next state
+		vector<long long> F;					//Set of features active
+		vector<long long> Fnext;              //Set of features active in next state
 		vector<float> Q;               //Q(a) entries
 		vector<float> Qnext;           //Q(a) entries for next action
-		vector<vector<float> > e;      //Eligibility trace
-		vector<vector<float> > w;      //Theta, weights vector
-		vector<vector<int> >nonZeroElig;//To optimize the implementation
-        vector<vector<int> > featureSeen;
+    vector<unordered_map<long long, float> > e;       //Eligibility trace
+    vector<unordered_map<long long, float> > w;     //Theta, weights vector
+		//vector<vector<long long> >nonZeroElig;//To optimize the implementation
+        //vector<vector<int> > featureSeen;
     
 
 		/**
@@ -54,19 +57,19 @@ class SarsaLearner : public RLLearner{
  		* It updates the vector<double> Q assuming that vector<int> F is filled, as it sums just the weights
  		* that are active in F.
  		*/
-		void updateQValues(vector<int> &Features, vector<float> &QValues);
+		void updateQValues(vector<long long> &Features, vector<float> &QValues);
 		/**
  		* When using Replacing traces, all values not related to the current action are set to 0, while the
  		* values for the current action that their features are active are set to 1. The traces decay following
  		* the rule: e[action][i] = gamma * lambda * e[action][i]. It is possible to also define thresholding.
  		*/
-		void updateReplTrace(int action, vector<int> &Features);
+		void updateReplTrace(int action, vector<long long> &Features);
 		/**
  		* When using Replacing traces, all values not related to the current action are set to 0, while the
  		* values for the current action that their features are active are added 1. The traces decay following
  		* the rule: e[action][i] = gamma * lambda * e[action][i]. It is possible to also define thresholding.
  		*/
-		void updateAcumTrace(int action, vector<int> &Features);
+		void updateAcumTrace(int action, vector<long long> &Features);
         /**
         * Prints the weights in a file. Each line will contain a weight.
         */
