2d91dae6b362187dc3f1196f93e3dc251d7548f4
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2015-07-09 20:09:27 -0600

    Structure of the code that will do the dimensionality reduction with Eigen.

--- research/novelty/clean_start/svd/DimReduction.cpp
+++ research/novelty/clean_start/svd/DimReduction.cpp
@@ -1,3 +1,27 @@
 /* Author: Marlos C. Machado */
 
-void reduceDimensionalityOfEvents(){}
+#include <iostream>
+
+#include "DimReduction.hpp"
+
+using namespace std;
+using namespace Eigen;
+
+void centerMatrix(MatrixXi dataset, MatrixXf &centeredDataset){}
+
+void fillCenteringVector(vector<float> &centeringVector){}
+
+void fillWithTopEigenVectors(int k, vector<vector<float> > &eigenVectors){}
+
+JacobiSVD<MatrixXf> reduceDimensionalityOfEvents(MatrixXi dataset, 
+	vector<float> &centeringVector, vector<vector<float> > &eigenVectors, int k){
+
+	MatrixXf centeredDataset;	
+	centerMatrix(dataset, centeredDataset);
+	//Proper SVD:
+	JacobiSVD<MatrixXf> svd(centeredDataset, ComputeThinU | ComputeThinV);
+	fillWithTopEigenVectors(k, eigenVectors);
+	fillCenteringVector(centeringVector);
+	
+	return svd;
+}
--- research/novelty/clean_start/svd/DimReduction.hpp
+++ research/novelty/clean_start/svd/DimReduction.hpp
@@ -1,3 +1,14 @@
 /* Author: Marlos C. Machado */
 
-void reduceDimensionalityOfEvents();
+#include <vector>
+#include <Eigen/SVD>
+#include <Eigen/Dense>
+
+void centerMatrix(Eigen::MatrixXi dataset, Eigen::MatrixXf &centeredDataset);
+
+void fillWithTopEigenVectors(int k, std::vector<std::vector<float> > &eigenVectors);
+
+void fillCenteringVector(std::vector<float> &centeringVector);
+
+Eigen::JacobiSVD<Eigen::MatrixXf> reduceDimensionalityOfEvents(Eigen::MatrixXi dataset, 
+	std::vector<float> &centeringVector, std::vector<std::vector<float> > &eigenVectors, int k);
