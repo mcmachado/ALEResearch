6ca8ff64fc06b91102658d6ba2acaa4a4380cb4f
alcinos <carion.nicolas@gmail.com>
2015-07-09 18:22:45 -0600

    value transfer relearn experiment

---
+++ src/launcher_vtr.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+base_path=`pwd`
+game=$1
+#romPath="/home/alcinos/roms/"
+romPath="/home/nicolas/Documents/Etudes/ENS/M1/Stage/emulateur/roms/"
+fullPath="${romPath}${game}.bin"
+cmd="./learner -s 3 -c conf.cfg -r ${fullPath}"
+echo "./mode_difficulty_getter ${fullPath} 1"
+for diff in `./mode_difficulty_getter ${fullPath} 1`;
+do
+    for mode in `./mode_difficulty_getter ${fullPath} 0`;
+    do
+        folder="${game}_BASIC_d${diff}_m${mode}"
+        mkdir -p ${folder}
+        cp conf.cfg ${folder}/conf.cfg
+        echo "DIFFICULTY_LEVEL = ${diff}" >> ${folder}/conf.cfg
+        echo "GAME_MODE = ${mode}" >> ${folder}/conf.cfg
+        cp skeleton.pbs ${folder}/run_d${diff}_m${mode}.pbs
+        cp learner ${folder}
+        cp mode_difficulty_getter ${folder}
+        echo "cd ${base_path}/${folder}" >>  ${folder}/run_d${diff}_m${mode}.pbs
+        echo "${cmd} &> out_${game}_d${diff}_m${mode}.out" >> ${folder}/run_d${diff}_m${mode}.pbs
+        bqsub ${folder}/run_d${diff}_m${mode}.pbs
+    done
+done
+
+
+bqsub --submit
