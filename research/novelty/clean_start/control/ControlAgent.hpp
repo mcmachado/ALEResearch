cbab4e71073750c56e38a204c4b545a14ddf0e1a
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2015-07-16 14:52:20 -0600

    Adding partial code that is the start for my Sarsa implementation.

--- research/novelty/clean_start/control/ControlAgent.hpp
+++ research/novelty/clean_start/control/ControlAgent.hpp
@@ -5,8 +5,21 @@
 #include "../control/Agent.hpp"
 #include "../common/Parameters.hpp"
 
-void learnOptionsDerivedFromEigenEvents();
 
-void gatherSamplesFromRandomTrajectories(ALEInterface& ale, Parameters *param, Agent &agent, int iter, vector<vector<bool> > &dataset);
-
+//Gathering data to obtain eigenvectors:
 int playGame(ALEInterface& ale, Parameters *param, int iter, vector<vector<bool> > &dataset);
+
+void gatherSamplesFromRandomTrajectories(ALEInterface& ale, Parameters *param, Agent &agent,
+	vector<vector<bool> > &dataset, int iter);
+
+//Learning Options:
+int argmax(std::vector<float> array);
+
+void learnOptions(ALEInterface &ale, Parameters *param, Agent &agent, RAMFeatures ramFeatures,
+	BPROFeatures bproFeatures, int option);
+
+void cleanTraces(vector<vector<float> > &e, vector<vector<int> > &nonZeroElig);
+
+void learnOptionsDerivedFromEigenEvents(ALEInterface &ale, Parameters *param,
+	Agent &agent, std::vector<float> &datasetMeans, std::vector<float> &datasetStds,
+	std::vector<std::vector<float> > &eigenVectors, vector<vector<vector<float> > > learnedOptions, int iter);
