8e38077055bfb0d05a7b4a59413e2d13b6113d00
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2015-07-21 11:33:52 -0600

    Adding more code to my fresh start. I still need to finish this implementation.

--- research/novelty/clean_start/control/Agent.hpp
+++ research/novelty/clean_start/control/Agent.hpp
@@ -17,15 +17,31 @@ class Agent{
 		BPROFeatures bproFeatures;
 		ActionVect actions; //Basic actions
 		std::vector<float> freqOfBitFlips; //[0:1023] transitions 0->1; [1024:2048] transitions 1->0
-		std::vector<std::vector<std::vector<float> > > w;  //Theta, weights vector
 		int numberOfAvailActions, numberOfOptions, numberOfPrimitiveActions;
+
+		vector<vector<float> > e;       //Eligibility trace
+		vector<vector<int> >nonZeroElig; //To optimize the implementation
+		/* This is the theta vector, the one containing the weights that really try
+		to maximize the external rewards (score). They are learned only at the end.
+		What we really learn during each iteration are the options, which the learned
+		weights are not w, but learnedOptions*/
+		std::vector<std::vector<std::vector<float> > > w;
+		/* This matrix is initially empty, but each new learned option is going to add its
+		learned set of weights on it. Therefore, when initialized it will be 0x0. After learnined
+		the first set of options (e.g. 5) it will be 5 x |A| x |F|, where |A| is 18 and |F| is the
+		size of the feature vector. In the second iteration it be concatenated to another matrix
+		with dimensions 5 x |A + 5| x |F|. */
+		vector<vector<vector<float> > > learnedOptions;
 
 		Agent(ALEInterface& ale, Parameters *param);
 		int playActionUpdatingAvg(ALEInterface& ale, Parameters *param, int &frame, 
 			int nextAction, int iter, vector<vector<bool> > &dataset);
 
+		void cleanTraces();
+		void sanityCheck(vector<float> &QValues);
 		int epsilonGreedy(vector<float> &QValues, float epsilon);
-		void updateQValues(vector<int> &Features, vector<float> &QValues, int option);
+		void updateReplTrace(Parameters *param, int action, vector<int> &Features);
+		void updateQValues(vector<vector<float> > &learnedOptions, vector<int> &Features, vector<float> &QValues, int option);
 	private:
 		Agent();
 		void updateAverage(Parameters *param, vector<bool> Fprev, vector<bool> F, 
