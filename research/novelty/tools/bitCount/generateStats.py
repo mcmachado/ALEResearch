1131b770beb0246857c80162443d37f8d889329c
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2015-07-23 09:30:39 -0600

    Script that generates useful statistics about behavior.

---
+++ research/novelty/tools/bitCount/generateStats.py
@@ -0,0 +1,34 @@
+import sys
+import csv
+
+## Reading the input
+if len(sys.argv) != 2:
+	print 'Usage: python', sys.argv[0], 'file_to_read'
+	exit(1)
+inputFile = sys.argv[1]
+
+#Reading CSV file and storing it in data
+data     = []
+str_data = []
+
+with open(inputFile, 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+    	data.append(row)
+    	str_data.append(''.join(row))
+
+#Now I can finally collect the statistics:
+totalNumFlips = 0
+totalNumSteps = len(data)
+
+for i, val in enumerate(data):
+	for j, val in enumerate(data[i]):
+		if i < len(data) - 1:
+			if data[i][j] != data[i+1][j]:
+				totalNumFlips += 1
+
+avgNumFlips = float(totalNumFlips)/float(totalNumSteps)
+numUniqStates = len(set(str_data))
+
+print "file , totalNumFlips , totalNumSteps , avgNumFlips , numUniqStates"
+print inputFile, ",", totalNumFlips, ",", totalNumSteps, ",", avgNumFlips, ",", numUniqStates
