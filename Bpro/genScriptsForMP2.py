f652057ca0e1aa210feef75b5e0d261884b7c591
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2015-07-23 13:59:35 -0600

    Fixing script to make it work on mp2.

--- Bpro/genScriptsForMP2.py
+++ Bpro/genScriptsForMP2.py
@@ -25,30 +25,31 @@ list_of_games = [
 'zaxxon']
 
 def generatePBSFiles(game_name):
-	f = open(game_name + '.pbs', 'w')
-	f.write('#!/bin/bash\n')
-	f.write('#PBS -S /bin/bash\n\n')
-	f.write('# Script for running\n\n')
+	for i in xrange(24):
+		f = open(game_name + '_' + str(i+1) + '.pbs', 'w')
+		f.write('#!/bin/bash\n')
+		f.write('#PBS -S /bin/bash\n\n')
+		f.write('# Script for running\n\n')
 
-	f.write('#PBS -l nodes=1\n')
-	f.write('#PBS -l mem=1024mb\n')
-	f.write('#PBS -l walltime=120:00:00\n')
-	f.write('#PBS -M machado@cs.ualberta.ca\n')
-	f.write('#PBS -t 1-24\n\n')
+		f.write('#PBS -l nodes=1\n')
+		f.write('#PBS -l mem=1024mb\n')
+		f.write('#PBS -l walltime=120:00:00\n')
+		f.write('#PBS -M machado@cs.ualberta.ca\n\n')
 
-	f.write('LD_LIBRARY_PATH=../../Arcade-Learning-Environment/\n')
-	f.write('export LD_LIBRARY_PATH\n\n')
-	f.write('cd $PBS_O_WORKDIR\n\n')
-	f.write('./learnerBpro -c ../conf/bpro.cfg -r ../roms/' + game_name + '.bin -s ${PBS_ARRAYID} -n checkPoints/' + game_name + '-${PBS_ARRAYID} > results/' + game_name + '/${PBS_ARRAYID}.out\n')
+		f.write('LD_LIBRARY_PATH=../../Arcade-Learning-Environment/\n')
+		f.write('export LD_LIBRARY_PATH\n\n')
+		f.write('cd $PBS_O_WORKDIR\n\n')
+		f.write('./learnerBpro -c ../conf/bpro.cfg -r ../roms/' + game_name + '.bin -s ' + str(i+1) + ' -n checkPoints/' + game_name + '-' + str(i+1) + ' > results/' + game_name + '/' + str(i+1) + '.out\n')
 
-	f.close()
+		f.close()
 
 f = open('submitEverything.sh', 'w')
 for game_name in list_of_games:
 	generatePBSFiles(game_name)
 	f.write('mkdir results/' + game_name + '\n')
-	f.write('bqsub ' + game_name + '.pbs\n')
-	f.write('echo Submitting ' + game_name + '.pbs\n')
+	for i in xrange(24):
+		f.write('bqsub ' + game_name + '_' + str(i+1) + '.pbs\n')
+	f.write('echo Submitting ' + game_name + '...\n')
 	f.write('bqsub --submit\n')
 
 f.close()
