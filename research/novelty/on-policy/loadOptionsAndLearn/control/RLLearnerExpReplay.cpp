8806a989746f0f1aff9614f16d0b360b0784f9cf
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2015-08-04 19:10:14 -0600

    Adding code that will be the base for the experience replay.

---
+++ research/novelty/on-policy/loadOptionsAndLearn/control/RLLearnerExpReplay.cpp
@@ -0,0 +1,158 @@
+#ifndef MATHEMATICS_H
+#define MATHEMATICS_H
+#include "../../../../../src/common/Mathematics.hpp"
+#endif
+
+#ifndef RL_LEARNER_H
+#define RL_LEARNER_H
+#include "RLLearnerExpReplay.hpp"
+#endif
+
+#include <fstream>
+
+using namespace std;
+
+RLLearner::RLLearner(ALEInterface& ale, Features *features, Parameters *param){
+	randomActionTaken   = 0;
+
+	gamma               = param->getGamma();
+	epsilon             = param->getEpsilon();
+	toUseOnlyRewardSign = param->getUseRewardSign();
+	toBeOptimistic      = param->getOptimisticInitialization();
+	
+	episodeLength       = param->getEpisodeLength();
+	numEpisodesEval     = param->getNumEpisodesEval();
+	totalNumberOfFramesToLearn = param->getLearningLength();
+
+	//Get the number of effective actions:
+	if(param->isMinimalAction()){
+		actions = ale.getMinimalActionSet();
+	}
+	else{
+		actions = ale.getLegalActionSet();
+	}
+	numOptions      = param->getNumOptionsLoad();
+	numBasicActions = actions.size();
+	numTotalActions = numBasicActions + numOptions;
+}
+
+int RLLearner::epsilonGreedy(vector<float> &QValues){
+	randomActionTaken = 0;
+
+	int action = Mathematics::argmax(QValues);
+	//With probability epsilon: a <- random action in A(s)
+	int random = rand();
+	if((random % int(nearbyint(1.0/epsilon))) == 0) {
+	//if((rand()%int(1.0/epsilon)) == 0){
+		randomActionTaken = 1;
+		action = rand() % QValues.size();
+	}
+	return action;
+}
+
+bool RLLearner::toInterruptOption(int currentOption, vector<int> &Features, vector<vector<float> > &w){
+
+	vector<float> Q(w.size(), 0.0);    //Q(a) entries
+	//Update Q-values for each possible action
+	for(int a = 0; a < w.size(); a++){
+		float sumW = 0;
+		for(unsigned int i = 0; i < Features.size(); i++){
+			sumW += w[a][Features[i]];
+		}
+		Q[a] = sumW;
+	}
+
+	int bestAction = Mathematics::argmax(Q);
+
+	if(Q[bestAction] - Q[currentOption] < 10e-3){
+		return false;
+	} else {
+		return true;
+	}
+}
+
+void RLLearner::playOption(ALEInterface& ale, int option, Features *features,
+	vector<float> &reward, vector<vector<vector<float> > > &learnedOptions,
+	vector<std::vector<float> > &w){
+	int numActionsInOption = learnedOptions[option].size();
+	int r_real = 0;
+	int currentAction;
+	vector<int> Fbpro;	                      	 //Set of features active
+	vector<float> Q(numActionsInOption, 0.0);    //Q(a) entries
+
+	float termProb = 0.0;
+
+	/* Rationale: Right now we have 'hierarchical' options. Options in the
+	   lower level can act over the 18 actions, and then they should take
+	   shorter than options that can call other options. Because the frame
+	   skip is 5, what we do is the following: the low-level options have
+	   a prob. of 0.05 of finishing, which corresponds on expectation to 20
+	   steps (x5 ~ 1.6s). High level options have a termination prob. of 0.2
+	   because if they call a low-level option they will have a termination
+	   probability of 0.2 * 0.05 in fact (100 steps x 5 ~ 8.3s). */
+	if(numActionsInOption > actions.size()){
+		termProb = 0.2;
+	}
+	else{
+		termProb = 0.05;
+	}
+
+	while(rand()%1000 > 1000 * termProb && !ale.game_over()){
+		//Get state and features active on that state:		
+		Fbpro.clear();
+		features->getActiveFeaturesIndices(ale.getScreen(), ale.getRAM(), Fbpro);
+
+		//Update Q-values for each possible action
+		for(int a = 0; a < numActionsInOption; a++){
+			float sumW = 0;
+			for(unsigned int i = 0; i < Fbpro.size(); i++){
+				sumW += learnedOptions[option][a][Fbpro[i]];
+			}
+			Q[a] = sumW;
+		}
+
+		currentAction = epsilonGreedy(Q);
+
+		if(toInterruptOption(currentAction, Fbpro, w)){
+			return;
+		}
+		/* Now things get nasty. We need to do it recursively because one 
+		option can call another one. Hopefully everything is going to work.*/
+		this->act(ale, currentAction, features, reward, learnedOptions, w);
+	}
+}
+
+/**
+ * The first parameter is the one that is used by Sarsa. The second is used to
+ * pass aditional information to the running algorithm (like 'real score' if one
+ * is using a surrogate reward function).
+ */
+void RLLearner::act(ALEInterface& ale, int action, Features *features,
+	vector<float> &reward, vector<vector<vector<float> > > &learnedOptions,
+	vector<std::vector<float> > &w){
+	float r_alg = 0.0, r_real = 0.0;
+
+	if(action < numBasicActions){
+		r_real = ale.act(actions[action]);
+	} 
+	else{
+		int option_idx = action - numBasicActions;
+		playOption(ale, option_idx, features, reward, learnedOptions, w);
+	}
+	/* Here I am letting the option return a single reward and then I am
+	 normalizing over it. I don't know if it is not better to normalize
+	 each step of the option. */
+	if(r_real != 0.0){
+		if(!sawFirstReward){
+			firstReward = std::abs(r_real);
+			sawFirstReward = 1;
+		}
+	}
+	if(sawFirstReward){
+		r_alg = r_real/firstReward;
+	}
+
+	reward[0] += r_alg;
+	reward[1] += r_real;
+}
+
---
+++ research/novelty/on-policy/loadOptionsAndLearn/control/RLLearnerExpReplay.hpp
@@ -0,0 +1,112 @@
+/****************************************************************************************
+** Abstract class that needs to be implemented by any ALE agent. It defines the method 
+** that controls the agent, forcing it to act. By using it we ensure that any approach,
+** based in RL can be easily run, following the same pattern.
+** 
+** Author: Marlos C. Machado
+***************************************************************************************/
+
+#ifndef AGENT_H
+#define AGENT_H
+#include "AgentExtended.hpp"
+#endif
+
+#ifndef RAM_FEATURES_H
+#define RAM_FEATURES_H
+#include "../../../../../src/features/RAMFeatures.hpp"
+#endif
+
+class RLLearner : public Agent{
+	protected:
+
+		ActionVect actions;
+
+		std::vector<float> option;
+		std::vector<float> mean;
+		std::vector<float> std;
+		
+		float gamma, epsilon;
+		float firstReward;
+		bool   sawFirstReward;
+
+		int    toUseOnlyRewardSign, toBeOptimistic;
+		int    randomActionTaken, numBasicActions;
+		int    numTotalActions, numOptions;
+		int    episodeLength, numEpisodesEval;
+		int    totalNumberOfFramesToLearn;
+
+		/**
+ 		* It acts in the environment and makes the proper operations in the reward signal (normalizing,
+ 		* being optimistic, etc).
+ 		*
+ 		* @param ALEInterface& ale Arcade Learning Environment interface: object used to define agents'
+ 		*        actions, obtain simulator screen, RAM, etc.
+ 		* @param int action action to be taken
+ 		*
+ 		* @param vector<double>& reward this vector is used to return, by reference, the reward observed
+ 		* by executing the action defined as parameter. This vector has two positions: in the first position
+ 		* the reward to be used by the RL algorithm is returned; in the second position, the game score is
+ 		* returned.
+ 		*/
+		void act(ALEInterface& ale, int action, Features *features, std::vector<float> &reward, 
+			std::vector<std::vector<std::vector<float> > > &learnedOptions, std::vector<std::vector<float> > &w);
+
+		/**
+ 		* Implementation of an epsilon-greedy function. Epsilon is defined in the constructor,
+ 		* in the argument Parameters *param
+ 		*
+ 		* @return int action to be taken
+ 		*/
+		int epsilonGreedy(std::vector<float> &QValues);
+
+		/**
+		* Constructor to be used by the RL classes to save the parameters that
+		* will be used by other methods.
+		*
+		* @param ALEInterface& ale Arcade Learning Environment interface: object used to define agents'
+ 		*        actions, obtain simulator's screen, RAM, etc.
+ 		* @param Parameters *param object containing the parameters passed to the algorithm, both by
+ 		*        file and command line.
+ 		*
+		*/
+		RLLearner(ALEInterface& ale, Features *features, Parameters *param);
+
+		void playOption(ALEInterface& ale, int option, Features *features, std::vector<float> &reward, 
+			std::vector<std::vector<std::vector<float> > > &learnedOptions, std::vector<std::vector<float> > &w);
+
+		bool toInterruptOption(int action, std::vector<int> &Features, std::vector<std::vector<float> > &w);
+
+	public:
+	   /**
+ 		* Pure virtual method that needs to be implemented by any RL agent.
+ 		*
+ 		* TODO: it may be useful to return something for the caller, as the total reward or policy. 
+ 		*       Additionally, it may be important to persist what was learned, maybe with another
+ 		*       pure virtual method?
+ 		*
+ 		* @param ALEInterface& ale Arcade Learning Environment interface: object used to define agents'
+ 		*        actions, obtain simulator's screen, RAM, etc.
+ 		* @param Features *features object that defines what feature function that will be used by the RL
+ 		*        agents.
+ 		*/
+		virtual void learnPolicy(ALEInterface& ale, Features *features, std::vector<std::vector<std::vector<float> > > &learnedOptions) = 0;
+
+		/**
+ 		* Pure virtual method that needs to be implemented by any agent. Once the agent learned a
+ 		* policy it executes this policy for a given number of episodes. The policy is stored in
+ 		* the class' object.
+ 		*
+ 		* TODO it may be useful to return something for the caller, as an indicator of performance. 
+ 		*
+ 		* @param ALEInterface& ale Arcade Learning Environment interface: object used to define agents'
+ 		*        actions, obtain simulator screen, RAM, etc.
+ 		* @param Features *features object that defines what feature function that will be used by the RL
+ 		*        agents. It may be null for other approaches as in Planning.
+ 		*/
+		virtual void evaluatePolicy(ALEInterface& ale, Features *features) = 0;
+
+		/**
+		* Destructor, not necessary in this class.
+		*/
+		virtual ~RLLearner(){};
+};
