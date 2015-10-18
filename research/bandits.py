5b7c17da4a53cc455787a1414dc245f830b8722c
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2015-10-17 18:21:13 -0600

    Adding simple bandits code.

---
+++ research/bandits.py
@@ -0,0 +1,114 @@
+import sys
+import random as r
+import numpy as np
+
+class Bandits:
+	numArms = 2
+	mus     = [1.0, 2.0]
+	sigmas  = [0.1, 20.0]
+
+	def __init__(self, s, numIterations):
+		np.random.seed(seed=s)
+		self.arms    = []
+		self.currIdx = []
+
+		for i in xrange(self.numArms):
+			self.currIdx.append(0)
+			self.arms.append([])
+			self.arms[i] = np.random.normal(self.mus[i], self.sigmas[i], numIterations + 1)
+
+	def pullArm(self, i):
+		assert(i < self.numArms)
+		self.currIdx[i] += 1
+		return self.arms[i][self.currIdx[i] - 1]
+
+	def resetEnv(self):
+		for i in xrange(self.numArms):
+			self.currIdx[i] = 0
+
+def UCB1(b, numIterations):
+	n          = []
+	avg        = []
+	armValue   = []
+	acumReturn = 0
+
+
+	for i in xrange(b.numArms):
+		n.append(1)
+		armValue.append(0)
+		avg.append(b.pullArm(i))
+
+	for t in xrange(2, numIterations + 2):
+		assert(t == sum(n))
+		for i in xrange(b.numArms):
+			armValue[i] = avg[i] + np.sqrt(2 * np.log(t/n[i]))
+
+		i           = np.argmax(armValue)
+		reward      = b.pullArm(i)
+		acumReturn += reward
+		avg[i]      = (reward + avg[i] * n[i])/(n[i] + 1)
+		n[i]       += 1
+
+	return acumReturn
+
+def epsilonGreedy(theta, epsilon = 0.05):
+	argmax = np.argmax(theta)
+
+	if r.randint(1, 100) < int(epsilon * 100):
+		return r.randint(0, len(theta)-1)	
+	else:
+		return argmax
+
+def SARSA(b, numIterations):
+
+	phi   = []
+	theta = []
+	acumReturn = 0
+	alpha      = 0.1
+
+	for i in xrange(b.numArms):
+		phi.append(0.0)
+		theta.append(0.0)
+
+	for t in xrange(numIterations - 1):
+		i = epsilonGreedy(theta)
+		
+		reward = b.pullArm(i)
+		acumReturn += reward
+		theta[i] = theta[i] + alpha * (reward + 0 - theta[i])
+
+	return acumReturn
+
+def __init__():
+
+	res_MAX   = 0.0
+	res_UCB   = 0.0
+	res_SARSA = 0.0
+
+	numSeeds      = 100
+	numIterations = 1000
+
+	b = []
+	for s in xrange(1, numSeeds+1):
+		b.append(Bandits(s, numIterations))
+
+	for s in xrange(1, numSeeds+1):
+		seed          = s
+		r.seed(seed)
+
+		maxReturn = 0
+		for i in xrange(numIterations):
+			maxReturn += max(b[s-1].pullArm(0), b[s-1].pullArm(1))
+
+		res_MAX += maxReturn
+		b[s-1].resetEnv()
+		res_UCB += UCB1(b[s-1], numIterations)
+		b[s-1].resetEnv()
+		res_SARSA += SARSA(b[s-1], numIterations)
+		b[s-1].resetEnv()
+		
+	print 'Max return:', res_MAX/numSeeds
+	print 'UCB-1     :', res_UCB/numSeeds
+	print 'SARSA     :', res_SARSA/numSeeds
+
+__init__()
